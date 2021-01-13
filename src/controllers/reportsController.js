018bce2531d363c3a449150a27133ac9e0e897a2
Enmanuel-Leon <45642674+enmanuel23x@users.noreply.github.com>
2021-01-13 09:47:41 -0400

    fix

--- src/controllers/reportsController.js
+++ src/controllers/reportsController.js
@@ -77,7 +77,7 @@ module.exports = {
             if( SellerName != undefined){
                 query += ` AND rpt3_seller RLIKE "` + SellerName + `"`;
             }
-            query += `order by rpt3_group asc limit 20`;
+            query += `order by rpt3_client_code asc limit 20`;
 
             const result = await pool.query(query)
             res.json(result)
@@ -107,7 +107,7 @@ module.exports = {
                 (select sum(CAST(rpt3_month_sales AS DECIMAL(10,2))) from copyoic.report_3 where rpt3_client_code = a.rpt3_client_code) as sum_month_sales,
                 (select sum(CAST(rpt3_scope_perc AS DECIMAL(10,2))) from copyoic.report_3 where rpt3_client_code = a.rpt3_client_code) as sum_scope_perc,
                 FORMAT((select (sum(CAST(rpt3_scope_perc AS DECIMAL(10,2)))/(select count(rpt3_scope_perc) from copyoic.report_3 where rpt3_client_code = a.rpt3_client_code)) as a from copyoic.report_3 where rpt3_client_code = a.rpt3_client_code),2) as prom_scope_perc,
-                (select count(rpt3_scope_perc) from copyoic.report_3 where rpt3_client_code = a.rpt3_client_code) as count_scope_perc FROM copyoic.report_3 as a WHERE MONTH(rpt3_date) = MONTH(NOW()) AND YEAR(rpt3_date) = YEAR(NOW())`
+                (select count(rpt3_avg_sales) from copyoic.report_3 where rpt3_client_code = a.rpt3_client_code) as count_scope_perc FROM copyoic.report_3 as a WHERE MONTH(rpt3_date) = MONTH(NOW()) AND YEAR(rpt3_date) = YEAR(NOW())`
                 //let query = `select * from report_3 WHERE MONTH(rpt3_date) = MONTH(NOW()) AND YEAR(rpt3_date) = YEAR(NOW())`
                 if( SellerActive != undefined){
                     query += ` AND rpt3_seller_active = "` + SellerActive + `"`;
@@ -115,7 +115,7 @@ module.exports = {
                 if( SellerName != undefined){
                     query += ` AND rpt3_seller RLIKE "` + SellerName + `"`;
                 }
-                query += `AND rpt3_client_code IN (${inClients}) order by rpt3_group asc`;
+                query += `AND rpt3_client_code IN (${inClients}) order by rpt3_client_code asc`;
                 const result = await pool.query(query)
     
                 res.json(result)
