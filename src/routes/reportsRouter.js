5aa72aed5c98b62f984ceb07fe61f3a33f886573
Enmanuel-Leon <45642674+enmanuel23x@users.noreply.github.com>
2021-02-04 14:36:27 -0400

    'report_5'

--- src/routes/reportsRouter.js
+++ src/routes/reportsRouter.js
@@ -6,9 +6,11 @@ const uriReport_1 = '/report_1';
 const uriReport_2 = '/report_2';
 const uriReport_3 = '/report_3';
 const uriReport_4 = '/report_4';
+const uriReport_5 = '/report_5';
 const uriSeller = '/seller';
 const uriReport4Brand = '/report_4/brand';
 const uriReport4Class = '/report_4/class';
+const uriReport5Client = '/report_5/client';
 const uriReport_3Top20Clients = '/report_3/top20clients';
 const uriReport_4Top20Clients = '/report_4/top20clients';
 
@@ -26,6 +28,9 @@ reportsRouter.route(uriReport_3Top20Clients)
 
 reportsRouter.route(uriReport_4)
     .post(reportsController.get_report_4)
+
+reportsRouter.route(uriReport_5)
+    .post(reportsController.get_report_5)
  
 reportsRouter.route(uriReport_4Top20Clients)
     .post(reportsController.get_report_4_top20_clients) 
@@ -38,5 +43,9 @@ reportsRouter.route(uriReport4Brand)
 
 reportsRouter.route(uriReport4Class)
     .get(reportsController.get_report_4_class)
+
+reportsRouter.route(uriReport5Client)
+    .get(reportsController.get_report_5_client)
+
 
 module.exports = reportsRouter;
