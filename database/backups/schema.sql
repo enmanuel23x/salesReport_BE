fa66a0cfbc0db3dd330fb105b458c7957a55fb91
Enmanuel-Leon <45642674+enmanuel23x@users.noreply.github.com>
2021-02-04 14:37:36 -0400

    'schema'

--- database/backups/schema.sql
+++ database/backups/schema.sql
@@ -1,9 +1,9 @@
-DROP SCHEMA IF EXISTS `copyOIC`;
-CREATE SCHEMA IF NOT EXISTS `copyOIC`;
-USE `copyOIC`;
+DROP SCHEMA IF EXISTS `copyoic`;
+CREATE SCHEMA IF NOT EXISTS `copyoic`;
+USE `copyoic`;
 
-DROP TABLE IF EXISTS `copyOIC`.`BASE_OIC2`;
-CREATE TABLE IF NOT EXISTS `copyOIC`.`BASE_OIC2` (
+DROP TABLE IF EXISTS `copyoic`.`BASE_OIC2`;
+CREATE TABLE IF NOT EXISTS `copyoic`.`BASE_OIC2` (
 	`CLIENTE` varchar(100) DEFAULT NULL,
 	`NOMBRE` varchar(256) DEFAULT NULL,
 	`VENDEDOR` varchar(50) DEFAULT NULL,
@@ -38,8 +38,8 @@ CREATE TABLE IF NOT EXISTS `copyOIC`.`BASE_OIC2` (
 	`COSTO` DECIMAL(38,8) DEFAULT NULL
 );
 
-DROP TABLE IF EXISTS `copyOIC`.`CLIENTE_OIC`;
-CREATE TABLE IF NOT EXISTS `copyOIC`.`CLIENTE_OIC` (
+DROP TABLE IF EXISTS `copyoic`.`CLIENTE_OIC`;
+CREATE TABLE IF NOT EXISTS `copyoic`.`CLIENTE_OIC` (
 	`CLIENTE` varchar(50) DEFAULT NULL,
 	`RAZON_SOCIAL` varchar(256) DEFAULT NULL,
 	`PROVINCIA` varchar(256) DEFAULT NULL,
@@ -53,8 +53,8 @@ CREATE TABLE IF NOT EXISTS `copyOIC`.`CLIENTE_OIC` (
 	`CONDICION_PAGO` INT(10) DEFAULT NULL
 );
 
-DROP TABLE IF EXISTS `copyOIC`.`OIC_VENDEDOR`;
-CREATE TABLE IF NOT EXISTS `copyOIC`.`OIC_VENDEDOR` (
+DROP TABLE IF EXISTS `copyoic`.`OIC_VENDEDOR`;
+CREATE TABLE IF NOT EXISTS `copyoic`.`OIC_VENDEDOR` (
 	`VENDEDOR` varchar(4) NOT NULL,
 	`NOMBRE` varchar(40) NOT NULL,
 	`E_MAIL` varchar(249) DEFAULT NULL,
@@ -64,15 +64,15 @@ CREATE TABLE IF NOT EXISTS `copyOIC`.`OIC_VENDEDOR` (
 	`U_ROL` varchar(150) DEFAULT NULL
 );
 
-DROP TABLE IF EXISTS `copyOIC`.`clients`;
-CREATE TABLE IF NOT EXISTS `copyOIC`.`clients` (
+DROP TABLE IF EXISTS `copyoic`.`clients`;
+CREATE TABLE IF NOT EXISTS `copyoic`.`clients` (
     `cli_id` int(11) NOT NULL AUTO_INCREMENT,
     `cli_name` varchar(45) NOT NULL,
     PRIMARY KEY (`cli_id`)
 );
 
-DROP TABLE IF EXISTS `copyOIC`.`users`;
-CREATE TABLE IF NOT EXISTS `copyOIC`.`users` (
+DROP TABLE IF EXISTS `copyoic`.`users`;
+CREATE TABLE IF NOT EXISTS `copyoic`.`users` (
     `usr_id` int(11) NOT NULL AUTO_INCREMENT,
     `usr_name` varchar(45) DEFAULT NULL,
     `usr_last_name` varchar(45) DEFAULT NULL,
@@ -82,11 +82,11 @@ CREATE TABLE IF NOT EXISTS `copyOIC`.`users` (
     `cli_id` int(11) NOT NULL,
     PRIMARY KEY (`usr_id`),
     KEY `cli_id` (`cli_id`),
-    CONSTRAINT `users_ibfk_1` FOREIGN KEY (`cli_id`) REFERENCES `copyOIC`.`clients` (`cli_id`) ON DELETE CASCADE ON UPDATE CASCADE
+    CONSTRAINT `users_ibfk_1` FOREIGN KEY (`cli_id`) REFERENCES `copyoic`.`clients` (`cli_id`) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-DROP TABLE IF EXISTS `copyOIC`.`habiles`;
-CREATE TABLE IF NOT EXISTS `copyOIC`.`habiles` (
+DROP TABLE IF EXISTS `copyoic`.`habiles`;
+CREATE TABLE IF NOT EXISTS `copyoic`.`habiles` (
   `hbl_id` int(11) NOT NULL AUTO_INCREMENT,
   `hbl_date` date DEFAULT NULL,
   `hbl_holidays` json DEFAULT NULL,
@@ -98,8 +98,8 @@ CREATE TABLE IF NOT EXISTS `copyOIC`.`habiles` (
 
 -- Tablas de Reportes
 
-DROP TABLE IF EXISTS `copyOIC`.`report_1`;
-CREATE TABLE IF NOT EXISTS `copyOIC`.`report_1` (
+DROP TABLE IF EXISTS `copyoic`.`report_1`;
+CREATE TABLE IF NOT EXISTS `copyoic`.`report_1` (
 	`rpt1_group` varchar(256) DEFAULT NULL,
 	`rpt1_avg_sales` varchar(256) DEFAULT NULL,
   	`rpt1_last_month` varchar(256) DEFAULT NULL,
@@ -112,8 +112,8 @@ CREATE TABLE IF NOT EXISTS `copyOIC`.`report_1` (
 	`rpt1_date` date DEFAULT NULL
 );
 
-DROP TABLE IF EXISTS `copyOIC`.`report_2`;
-CREATE TABLE IF NOT EXISTS `copyOIC`.`report_2` (
+DROP TABLE IF EXISTS `copyoic`.`report_2`;
+CREATE TABLE IF NOT EXISTS `copyoic`.`report_2` (
 	`rpt2_group` varchar(256) DEFAULT NULL,
 	`rpt2_avg_sales_weekly` varchar(256) DEFAULT NULL,
   	`rpt2_first_week` varchar(256) DEFAULT NULL,
@@ -124,8 +124,8 @@ CREATE TABLE IF NOT EXISTS `copyOIC`.`report_2` (
 	`rpt2_date` date DEFAULT NULL
 );
 
-DROP TABLE IF EXISTS `copyOIC`.`report_3`;
-CREATE TABLE IF NOT EXISTS `copyOIC`.`report_3` (
+DROP TABLE IF EXISTS `copyoic`.`report_3`;
+CREATE TABLE IF NOT EXISTS `copyoic`.`report_3` (
 	`rpt3_client_code` varchar(256) DEFAULT NULL,
 	`rpt3_group` varchar(256) DEFAULT NULL,
 	`rpt3_brand` varchar(256) DEFAULT NULL,
@@ -138,8 +138,8 @@ CREATE TABLE IF NOT EXISTS `copyOIC`.`report_3` (
 	`rpt3_date` date DEFAULT NULL
 );
 
-DROP TABLE IF EXISTS `copyOIC`.`report_4`;
-CREATE TABLE IF NOT EXISTS `copyOIC`.`report_4` (
+DROP TABLE IF EXISTS `copyoic`.`report_4`;
+CREATE TABLE IF NOT EXISTS `copyoic`.`report_4` (
 	`rpt4_client_code` varchar(256) DEFAULT NULL,
 	`rpt4_group` varchar(256) DEFAULT NULL,
 	`rpt4_article` varchar(256) DEFAULT NULL,
@@ -156,10 +156,13 @@ CREATE TABLE IF NOT EXISTS `copyOIC`.`report_4` (
 
 DROP TABLE IF EXISTS `copyoic`.`report_5`;
 CREATE TABLE IF NOT EXISTS `report_5` (
-  `rpt5_description` varchar(256) DEFAULT NULL,
+  `rpt5_group` varchar(256) DEFAULT NULL,
   `rpt5_article_code` varchar(256) DEFAULT NULL,
+  `rpt5_class` varchar(256) DEFAULT NULL,
+  `rpt5_description` varchar(256) DEFAULT NULL,
   `rpt5_vtaCantidad_1` varchar(256) DEFAULT NULL,
   `rpt5_vtaCantidad_2` varchar(256) DEFAULT NULL,
   `rpt5_vtaCantidad_3` varchar(256) DEFAULT NULL,
-  `rpt5_vtaCantidad_4` varchar(256) DEFAULT NULL
-
+  `rpt5_vtaCantidad_4` varchar(256) DEFAULT NULL,
+  `rpt5_date` varchar(256) DEFAULT NULL
+);
