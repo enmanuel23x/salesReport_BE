899f90b5e8207d62dc1af66f3fd13fa259fadde1
Endrina Toledo <etoledo@intelix.biz>
2021-02-12 12:59:47 -0400

    ajuste en la ruta get_report_1
    ajuste de la data resultante del filtro de vendedor aplicado desde el rol de gerente

--- src/controllers/reportsController.js
+++ src/controllers/reportsController.js
@@ -4,7 +4,7 @@ const pool = require('../../database/conn/database');
 module.exports = {
 
     async get_report_1 (req, res, next) {
-     
+        
         try {
             
             const { ABC, SellerActive, SellerName, Rol, SellerCode,UsrId } = req.body;
@@ -29,6 +29,8 @@ module.exports = {
                     terms += ` AND rpt1_seller RLIKE "` + SellerName + `"`;
 
                 }else{
+                    
+
                     const vendors = await pool.query(`SELECT usr_code_seller FROM copyoic.users where usr_id_supervisor = '${UsrId}'`)                    
                     if(vendors.length !== 0){
                         data += `(`
@@ -50,8 +52,29 @@ module.exports = {
             if (Rol == '1'){              //rol de administrador
                 
                 if( SellerName != undefined){
-                    terms += ` AND rpt1_seller RLIKE "` + SellerName + `"`;  
+                    let sellerName_ ='';
+                    let vendors_ = []
+                    let idSupervisor = ''
+                    let arrayVendors = SellerName.split('|'); 
+                    for (let index = 0; index < arrayVendors.length; index++) {
+                        if(arrayVendors[index].substr(0,1) === '_'){
+                            idSupervisor = arrayVendors[index].substr(1)
+                            vendors_ = await pool.query(`select usr_name as usrName, usr_last_name as usrLastName
+                            from users where usr_id_supervisor = '${idSupervisor}' and usr_status = 0`)                    
+                            
+                        }else{
+                            sellerName_+= `${arrayVendors[index]}|`
+                        }
+                        if(vendors_.length !== 0){
+                            vendors_.forEach(x => {
+                                sellerName_+= `${x.usrName} ${x.usrLastName}|`
+                            })
+                        }
+                    }                    
+                    sellerName_ = sellerName_.slice(0, -1);
+                    terms += ` AND rpt1_seller RLIKE "` + sellerName_ + `"`;  
                   }else{
+
                       const vendors = await pool.query(`SELECT usr_code_seller FROM copyoic.users where usr_rol = '3'`)                    
                       if(vendors.length !== 0){
                           data += `(`
